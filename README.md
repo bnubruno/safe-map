a2c36fca08c2e7353ba73e0877fdd401e5a96482
Bruno Muehlbauer de Souza <bruno.muehlbauer@contaazul.com>
2019-02-15 09:28:19 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Safe Map
+
+[![](https://jitpack.io/v/jitpack/safe-map.svg?label=Release)](https://jitpack.io/#jitpack/safe-map)
+
+[https://jitpack.io/#jitpack/safe-map](https://jitpack.io/#jitpack/safe-map)
+
+To install the library add: 
+ 
+   ```gradle
+   repositories { 
+        jcenter()
+        maven { url "https://jitpack.io" }
+   }
+   dependencies {
+         compile 'com.github.jitpack:safe-map:1.0.5'
+   }
+   ```  
+
---
+++ src/main/java/com/github/bnubruno/safe/Safe.java
@@ -0,0 +1,26 @@
+package com.github.bnubruno.safe;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+public class Safe {
+
+	public static <I, O, M> O safeConverter(Optional<I> object, Function<I, M> mapFunction,
+			Function<M, O> converterFunction) {
+		return safeConverter( object, mapFunction, converterFunction, null );
+	}
+
+	public static <I, O, M> O safeConverter(Optional<I> object, Function<I, M> getAttribute, Function<M, O> converter,
+			O defaultValue) {
+		return object.map( getAttribute ).map( converter ).orElse( defaultValue );
+	}
+
+	public static <O, I> O safeMap(Optional<I> object, Function<I, O> getAttribute, O defaultValue) {
+		return object.map( getAttribute ).orElse( defaultValue );
+	}
+
+	public static <I, O> I safeMap(Optional<O> object, Function<O, I> getAttribute) {
+		return safeMap( object, getAttribute, (I) null );
+	}
+
+}
